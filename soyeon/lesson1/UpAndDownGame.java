973a1fdd4f589477b454f9beb627fdc42d6f0aa6
hanssoy <soyeonesther@naver.com>
2022-10-10 21:04:50 +0900

    1st UpAndDownGame

---
+++ soyeon/lesson1/UpAndDownGame.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class UpAndDownGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+
+        System.out.println("[UP AND DOWN GAME]");
+        int random = rand.nextInt(50)+1;
+
+        while (true) {
+            System.out.print("1과 50 사이의 정수를 입력하세요.: ");
+            int inputnum = scanner.nextInt();
+            if (inputnum == -1) {
+                System.out.print("GAME OVER");
+                break;
+            }
+            if (inputnum > random) {
+                System.out.println("DOWN");
+            } else if (inputnum < random) {
+                System.out.println("UP");
+            } else {
+                System.out.println("정답입니다!");
+            }
+        }
+    }
+
+
+}
